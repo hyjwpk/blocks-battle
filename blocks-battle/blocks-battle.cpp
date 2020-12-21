d0fa65f1db67093e1d500b9aa0bc39363354adf0
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2020-12-21 15:05:14 +0800

    update

---
+++ blocks-battle/blocks-battle.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "blocks-battle.h"
+
+int main(void)
+{
+	initgraph(WIDTH1, HIGH, EW_NOCLOSE);//禁用绘图窗口的关闭按钮。
+	SetWindowText(GetHWnd(), "BLOCKS-BATTLE");
+	CreateThread(NULL, 0, Thread_GetMouseMsg, NULL, 0, NULL);
+	menu();
+	return 0;
+}
